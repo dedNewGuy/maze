4e13b7faf4318965aa98d72ff2b784ee16c66894
amin kafri <aminkafrinewgen@gmail.com>
2025-07-10 00:08:53 +0800

    draw line

--- maze.c
+++ maze.c
@@ -2,12 +2,17 @@
 
 #include "raylib.h"
 
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+#define WIN_TITLE "MAZE SOLVER"
+
 int main(void)
 {
-	InitWindow(800, 600, "Maze");  // Initialize window and OpenGL context
+	InitWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);  // Initialize window and OpenGL context
 								   
 	while (!WindowShouldClose()) {
 		BeginDrawing();
+		DrawLineEx((Vector2){0, 0}, (Vector2){WIN_WIDTH, WIN_HEIGHT}, 2, WHITE);
 		EndDrawing();
 	}
 
