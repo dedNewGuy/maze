481f1971f0b3bb7eec30d60ebfa2fc05415295e7
amin kafri <aminkafrinewgen@gmail.com>
2025-07-10 00:54:47 +0800

    generate grid and dump grid (print)

--- maze.c
+++ maze.c
@@ -1,21 +1,86 @@
 #include <stdio.h>
 
 #include "raylib.h"
+#include "util.h"
 
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 600
 #define WIN_TITLE "MAZE SOLVER"
 
-int main(void)
+typedef struct {
+	int x;
+	int y;
+	int width;
+	int height;
+} Cell_T;
+
+Cell_T cell_new(int x, int y, int cell_w, int cell_h)
 {
-	InitWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);  // Initialize window and OpenGL context
-								   
-	while (!WindowShouldClose()) {
-		BeginDrawing();
-		DrawLineEx((Vector2){0, 0}, (Vector2){WIN_WIDTH, WIN_HEIGHT}, 2, WHITE);
-		EndDrawing();
+	Cell_T cell = {
+		.x = x,
+		.y = y,
+		.width = cell_w, 
+		.height = cell_h,
+	};
+	return cell;
+}
+
+typedef struct {
+	Cell_T *items;
+	int count;
+	int cap;
+} Grid_T;
+
+#define GRID_X_OFFSET 10
+#define GRID_Y_OFFSET 10
+
+Grid_T grid_new(int win_w, int win_h, int cell_w, int cell_h)
+{
+	Grid_T grid = {0};
+
+	int x_offset = GRID_X_OFFSET;
+	int y_offset = GRID_Y_OFFSET;
+	int grid_w = win_w - (2*x_offset);
+	int grid_h = win_h - (2*y_offset);
+
+	int x_cell_count = grid_w / cell_w;
+	int y_cell_count = grid_h / cell_h;
+
+	for (int row = 0; row < y_cell_count; ++row) {
+		for (int col = 0; col < x_cell_count; ++col) {
+			Cell_T cell = cell_new(col*cell_w, row*cell_h, cell_w, cell_h);
+			min_da_append(&grid, cell);
+		}
 	}
 
-	CloseWindow();
+	return grid;
+}
+
+void gen_dump(Grid_T grid)
+{
+	for (int i = 0; i < grid.count; ++i) {
+		if (i % 5 == 0) {
+			printf("\n");
+		}
+		Cell_T cell = grid.items[i];
+		printf("{%d,%d,%d,%d} ", cell.x, cell.y, cell.width, cell.height);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	// InitWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE); 
+
+	Grid_T grid = grid_new(WIN_WIDTH, WIN_HEIGHT, 4, 4);
+	gen_dump(grid);
+
+	// while (!WindowShouldClose()) {
+	// 	BeginDrawing();
+	// 	DrawLineEx((Vector2){0, 0}, (Vector2){WIN_WIDTH, WIN_HEIGHT}, 2, WHITE);
+	// 	EndDrawing();
+	// }
+	//
+	// CloseWindow();
 	return 0;
 }
